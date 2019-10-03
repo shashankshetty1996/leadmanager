091a1ed6a02c06be0d55fe126ff826439bf9e921
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-10-03 15:43:17 +0530

    Redux setup and listing all leads

--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,13 +1,57 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import { getLeads } from "../../actions/leads.actions";
 
 export class Leads extends Component {
+  static propTypes = {
+    leads: PropTypes.array.isRequired,
+    getLeads: PropTypes.func.isRequired
+  };
+
+  componentDidMount() {
+    this.props.getLeads();
+  }
+
   render() {
     return (
-      <div>
-        <h1>Leads list</h1>
-      </div>
+      <Fragment>
+        <h2>Leads list</h2>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Message</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.leads.map(lead => (
+              <tr key={lead.id}>
+                <td>{lead.id}</td>
+                <td>{lead.name}</td>
+                <td>{lead.email}</td>
+                <td>{lead.message}</td>
+                <td>
+                  <button className="btn btn-danger btm-sm">delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </Fragment>
     );
   }
 }
 
-export default Leads;
+const mapStateToProps = state => ({
+  leads: state.leads.leads
+});
+
+export default connect(
+  mapStateToProps,
+  { getLeads }
+)(Leads);
---
+++ leadmanager/frontend/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import leads from "./leads.reducers";
+
+export default combineReducers({
+  leads
+});
