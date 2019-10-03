4bcec2d41ba281d88e3b8330808020bc76b8a42a
Shashank S Shetty <smile.shashank.s.shetty1996@gmail.com>
2019-10-03 15:01:35 +0530

    Basic dashboard setup

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Form from "./Forms";
+import Leads from "./Leads";
+
+const Dashboard = () => {
+  return (
+    <Fragment>
+      <Form />
+      <Leads />
+    </Fragment>
+  );
+};
+
+export default Dashboard;
